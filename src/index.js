ba74639ac9bcaf4a93050f7391cd571bc44b2c93
Komarudin <komarudin2110@gmail.com>
2019-11-07 15:08:58 +0700

    t

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-app.use(express.static(path.join(__dirname, '../public/uploads')));
+app.use('/public', express.static('public'));
+// app.use(express.static(path.join(__dirname, '../public/uploads')));
 
 app.get('/', (req, res) => {
     res.send(`<h1>RUNNING AT PORT ${port}</h1>`)
